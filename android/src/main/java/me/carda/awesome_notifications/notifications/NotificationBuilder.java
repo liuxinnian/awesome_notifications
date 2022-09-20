cd7d05cd1fa42323046442d0dcddf8321bcf623d
Xinnian Liu <uplinux@gmail.com>
2022-09-20 11:03:05 +0800

    restore NotificationBuilder.java

--- android/src/main/java/me/carda/awesome_notifications/notifications/NotificationBuilder.java
+++ android/src/main/java/me/carda/awesome_notifications/notifications/NotificationBuilder.java
@@ -384,36 +384,36 @@ public class NotificationBuilder {
         }
 
         setSmallIcon(context, notificationModel, channel, builder);
-//
-//        setRemoteHistory(notificationModel, builder);
-//
-//        setGrouping(context, notificationModel, channel, builder);
-//
-//        setVisibility(context, notificationModel, channel, builder);
-//        setShowWhen(notificationModel, builder);
+
+        setRemoteHistory(notificationModel, builder);
+
+        setGrouping(context, notificationModel, channel, builder);
+
+        setVisibility(context, notificationModel, channel, builder);
+        setShowWhen(notificationModel, builder);
 
         setLayout(context, notificationModel, channel, builder);
-//
-//        setTitle(notificationModel, channel, builder);
-//        setBody(notificationModel, builder);
-//
-//        setAutoCancel(notificationModel, builder);
-//        setTicker(notificationModel, builder);
-//        setOnlyAlertOnce(notificationModel, channel, builder);
-//
-//        setLockedNotification(notificationModel, channel, builder);
-//        setImportance(channel, builder);
-//        setCategory(notificationModel, builder);
-//
-//        setSound(context, notificationModel, channel, builder);
-//        setVibrationPattern(channel, builder);
-//        setLights(channel, builder);
-//
-//        setSmallIcon(context, notificationModel, channel, builder);
-//        setLargeIcon(context, notificationModel, builder);
-//        setLayoutColor(context, notificationModel, channel, builder);
-//
-//        createActionButtons(context, notificationModel, channel, builder);
+
+        setTitle(notificationModel, channel, builder);
+        setBody(notificationModel, builder);
+
+        setAutoCancel(notificationModel, builder);
+        setTicker(notificationModel, builder);
+        setOnlyAlertOnce(notificationModel, channel, builder);
+
+        setLockedNotification(notificationModel, channel, builder);
+        setImportance(channel, builder);
+        setCategory(notificationModel, builder);
+
+        setSound(context, notificationModel, channel, builder);
+        setVibrationPattern(channel, builder);
+        setLights(channel, builder);
+
+        setSmallIcon(context, notificationModel, channel, builder);
+        setLargeIcon(context, notificationModel, builder);
+        setLayoutColor(context, notificationModel, channel, builder);
+
+        createActionButtons(context, notificationModel, channel, builder);
 
         PendingIntent pendingActionIntent = getPendingActionIntent(context, notificationModel, channel);
         PendingIntent pendingDismissIntent = getPendingDismissIntent(context, notificationModel, channel);
@@ -428,10 +428,10 @@ public class NotificationBuilder {
         if(androidNotification.extras == null)
             androidNotification.extras = new Bundle();
 
-//        updateTrackingExtras(notificationModel, channel, androidNotification.extras);
-//
-//        setWakeUpScreen(context, notificationModel);
-//        setCriticalAlert(context, channel);
+        updateTrackingExtras(notificationModel, channel, androidNotification.extras);
+
+        setWakeUpScreen(context, notificationModel);
+        setCriticalAlert(context, channel);
         setCategoryFlags(context, notificationModel, androidNotification);
 
         return androidNotification;
@@ -1170,10 +1170,9 @@ public class NotificationBuilder {
 
          notificationLayout.setTextViewText(R.id.btn_more, "Read more");
 
-        builder.setContent(notificationLayout);
-//         builder
-//                 .setCustomContentView(notificationLayout)
-//                 .setCustomBigContentView(notificationLayout);
+         builder
+                 .setCustomContentView(notificationLayout)
+                 .setCustomBigContentView(notificationLayout);
 
         return true;
      }
